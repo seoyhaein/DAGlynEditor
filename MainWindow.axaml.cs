eabb966e0dd5956b327bfcd0eccfbad72fc1e979
HeaInSeo <jwsnsl@knou.ac.kr>
2023-10-12 16:43:37 +0900

    초기 커밋.

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace DAGlynEditor
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
