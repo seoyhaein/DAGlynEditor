e99543cd881ba470a8e4f7c07f96e09670997e9f
HeaInSeo <jwsnsl@knou.ac.kr>
2023-10-24 17:27:48 +0900

    버그 발견

--- DAGlynEditorCanvas.cs
+++ DAGlynEditorCanvas.cs
@@ -7,12 +7,14 @@ using System.Diagnostics;
 using Avalonia.Input;
 using System.ComponentModel;
 using Avalonia.Remote.Protocol.Input;
+using System.Linq;
+using Avalonia.Controls;
 
 namespace DAGlynEditor
 {
     public class DAGlynEditorCanvas : OverlayLayer
     {
-        #region Viewport 화면에 이동에 사용될 예정, 일단 구현만 먼저 확인한다. 추후 코드 최적화 함.
+        #region Offset 화면이동.
         public static readonly StyledProperty<Point> OffsetProperty =
            AvaloniaProperty.Register<DAGlynEditorCanvas, Point>(nameof(Offset), default);
 
@@ -41,7 +43,7 @@ namespace DAGlynEditor
             PointerReleased += OnPointerReleased;
             PointerMoved += OnPointerMoved;
         }
-
+        
         protected override Size ArrangeOverride(Size finalSize)
         {
             var child = Children;
@@ -73,6 +75,7 @@ namespace DAGlynEditor
             }
             return default;
         }
+        
         // TODO 이거 자체에 문제가 있는 듯하다.
         // RX 를 적용해야 할듯.
         protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs change)
@@ -82,8 +85,8 @@ namespace DAGlynEditor
             if (change.Property == OffsetProperty && change.NewValue is Point pointValue && RenderTransform is TranslateTransform translateTransform)
             {
                 Debug.WriteLine($"Canvas ViewPort changed to: {change.NewValue}");
-                translateTransform.X = pointValue.X;
-                translateTransform.Y = pointValue.Y;
+                translateTransform.X = -pointValue.X;
+                translateTransform.Y = -pointValue.Y;
             }
         }
         // 라우트된 이벤트로 처리하지 않고 직접 처리함.
@@ -131,60 +134,46 @@ namespace DAGlynEditor
             }
         }
 
-        // 일단 여기서 마우스 이벤트를 처리하는 방향으로 한다. 왜 밖으로 빼는지을 이해가 안된다.
-       /* protected override void OnPointerPressed(PointerPressedEventArgs e)
+        // 추후 수정한다.Move, Release 에 배치할까? Release 에는 안해도 될 것 같지만 생각해보자. 일단 배치하자.
+        private void StartAnimation(Point newLocation)
         {
-            base.OnPointerPressed(e);
-            if (!e.GetCurrentPoint(this).Properties.IsLeftButtonPressed)
+            if (RenderTransform == null)
             {
-                return;
+                RenderTransform = new TranslateTransform();
             }
 
-            Focus();
-            e.Pointer.Capture(this);
-
-            if (this.Equals(e.Pointer.Captured))
+            if (Transitions == null)
             {
-                // 일단 여기서 Anchor 업데이트. 땜방코드
-                if (Container == null) return;
-                var check = UpdateAnchor();
-
-                // TODO 추후 로그로
-                if (!check) throw new InvalidOperationException("Failed to update the Anchor.");
-                
-                Debug.Print("OnPointerPressed");
-                e.Handled = true;
+                Transitions = new Transitions();
             }
-        }
 
-        protected override void OnPointerReleased(PointerReleasedEventArgs e)
-        {
-            base.OnPointerReleased(e);
-            if (this.Equals(e.Pointer.Captured))
+            // 임시로 일단 ViewportZoom, BringIntoViewSpeed, BringIntoViewMaxDuration 를 넣어둠.
+            double ViewportZoom = 1d;
+            double BringIntoViewSpeed = 1d;
+            double BringIntoViewMaxDuration = 1d;
+            double distance = ((Vector)(newLocation - Offset)).Length;
+            double duration = distance / (BringIntoViewSpeed + (distance / 10)) * ViewportZoom;
+            duration = Math.Max(0.1, Math.Min(duration, BringIntoViewMaxDuration));
+
+            // 기존 PointTransition 찾기
+            var existingTransition = Transitions.OfType<PointTransition>().FirstOrDefault(t => t.Property == OffsetProperty);
+
+            if (existingTransition != null)
             {
-                if (Thumb != null)
+                // 이미 존재하는 PointTransition의 Duration 업데이트
+                existingTransition.Duration = TimeSpan.FromSeconds(duration);
+            }
+            else
+            {
+                // 존재하지 않으면 새로운 PointTransition 생성 및 추가
+                var pointTransition = new PointTransition
                 {
-                    _thumbCenter = new Point(Thumb.Bounds.Width / 2, Thumb.Bounds.Height / 2);
-                }
-
-               
-                // capture 해제.
-                e.Pointer.Capture(null);
-                e.Handled = true;
+                    Property = OffsetProperty,
+                    Duration = TimeSpan.FromSeconds(duration)
+                };
+                Transitions.Add(pointTransition);
             }
-            Debug.Print("OnPointerReleased");
         }
 
-        protected override void OnPointerMoved(PointerEventArgs e)
-        {
-            base.OnPointerMoved(e);
-
-            if (_thumbCenter.HasValue)
-            {
-                Vector? offset = e.GetPosition(Thumb) - _thumbCenter.Value;
-               
-            }
-            e.Handled = true;
-        }*/
     }
 }
